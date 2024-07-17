45ea7dae2b82e8c3d6b0bbd84e69b8c904e26686
hye33 <128382222+hye33@users.noreply.github.com>
2024-07-17 14:01:40 +0900

    [Feat] Board 스터디 생성 시 입력값 체크
    react-native-toast-message 라이브러리 추가 및 Toast 디자인 수정.
    
    언어, 요일, 스터디 방식: null 체크
    채팅방명, 제목, 내용: 공백(space, tab) 외 입력값 존재 여부 체크
    모집 인원: 정수 여부 체크

--- app/screens/boardWriteScr.js
+++ app/screens/boardWriteScr.js
@@ -8,10 +8,38 @@ import DropDownPicker from 'react-native-dropdown-picker';
 import SingleSelectButton from '../components/SingleSelectButton';
 import MultiSelectButton from '../components/MultiSelectButton';
 import { Entypo } from '@expo/vector-icons';
+import Toast from 'react-native-toast-message';
 
-export const BoardWriteScreen = () => {
-    const [selectedWay, setSelectedWay] = useState('');
-    const [selectedDays, setSelectedDays] = useState([]);
+const toastConfig = {
+    default: ({ text1, props }) => (
+        <View style={{
+            paddingHorizontal: 40,
+            paddingVertical: 10,
+            opacity: 0.9,
+            backgroundColor: '#757575',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 20,
+            ...Platform.select({
+                ios: {
+                    shadowColor: "#000000",
+                    shadowOpacity: 0.45,
+                },
+                android: { elevation: 5 }
+            })
+        }}>
+            <Text style={{
+                color: '#ffffff'
+            }}>{text1}</Text>
+        </View>
+    )
+}
+
+const removeSpace = text => (text.replace(/\s/g, ''))
+
+export const BoardWriteScreen = ({navigation}) => {
+    const [selectedWay, setSelectedWay] = useState(null);
+    const [selectedDays, setSelectedDays] = useState(null);
     const [open, setOpen] = useState(false);
     const [language, setLanguage] = useState(null);
     const [items, setItems] = useState([
@@ -22,6 +50,11 @@ export const BoardWriteScreen = () => {
         { label: '스페인어', value: 'spanish' },
         { label: '불어', value: 'french' },
     ]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const [chatname, setChatname] = useState('');
+    const [recruit, setRecruit] = useState('');
+
     const [loaded, error] = useFonts({
         'Pretendard-Bold': require('../assets/fonts/Pretendard-Bold.ttf'),
         'Pretendard-Regular': require('../assets/fonts/Pretendard-Regular.ttf')
@@ -36,50 +69,136 @@ export const BoardWriteScreen = () => {
     if (!loaded && !error) {
         return null;
     }
+
+    const CreateStudy = () => {
+        if (language == null) {
+            Toast.show({
+                type: 'default',
+                text1: '언어를 선택해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (removeSpace(chatname) == '') {
+            Toast.show({
+                type: 'default',
+                text1: '채팅방명을 입력해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (removeSpace(recruit.toString()) == '') {
+            Toast.show({
+                type: 'default',
+                text1: '최대 정원을 입력해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (isNaN(recruit) || recruit.toString().includes('.')) {
+            Toast.show({
+                type: 'default',
+                text1: '최대 정원은 정수만 입력 가능합니다.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (Number(recruit) < 2 || Number(recruit) > 10) {
+            Toast.show({
+                type: 'default',
+                text1: '최대 정원은 2~10인입니다.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (removeSpace(title) == '') {
+            Toast.show({
+                type: 'default',
+                text1: '게시글 제목을 입력해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (removeSpace(content) == '') {
+            Toast.show({
+                type: 'default',
+                text1: '스터디 내용을 입력해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (selectedDays == null || selectedDays.includes(true) == false) {
+            Toast.show({
+                type: 'default',
+                text1: '요일을 선택해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else if (selectedWay == null) {
+            Toast.show({
+                type: 'default',
+                text1: '스터디 방식을 선택해 주세요.',
+                bottomOffset: 50,
+                position: 'bottom'
+            });
+        } else {
+            console.log('create study successful');
+            navigation.pop();
+        }
+    }
+
     return (
         <View style={{ backgroundColor: '#ffffff', flex: 1 }}>
+            <View style={{ marginHorizontal: 15 }}>
+                <DropDownPicker
+                    open={open}
+                    value={language}
+                    items={items}
+                    setOpen={setOpen}
+                    setValue={setLanguage}
+                    setItems={setItems}
+                    placeholder={'언어 선택'}
+                    textStyle={{ fontFamily: 'Pretendard-Bold', fontSize: 17, color: '#5678F0' }}
+                    containerStyle={{ width: 130 }}
+                    style={{ borderWidth: 0 }}
+                    dropDownContainerStyle={{ borderWidth: 0 }}
+                    showArrowIcon={true}
+                    showTickIcon={false}
+                    ArrowDownIconComponent={() => <Entypo name="chevron-small-down" size={24} color="#5678F0" />}
+                    ArrowUpIconComponent={() => <Entypo name="chevron-small-up" size={24} color="#5678F0" />}
+                />
+            </View>
 
-
+            <View style={{ marginHorizontal: 25, flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={style.GuideText}>채팅방명</Text>
+                <TextInput
+                    value={chatname}
+                    onChangeText={(text) => { setChatname(text) }}
+                    style={style.InfoText} placeholder='채팅방명을 입력해 주세요.' />
+            </View>
+            <View style={{ marginHorizontal: 25, flexDirection: 'row', alignItems: 'center' }}>
+                <Text style={style.GuideText}>최대정원</Text>
+                <TextInput
+                    value={recruit}
+                    onChangeText={(number) => { setRecruit(number) }}
+                    style={style.GuideTextstyle}
+                    keyboardType='number-pad'
+                    placeholder='채팅방 정원을 입력해 주세요. (2~10)' />
+            </View>
             <ScrollView>
-                <View style={{ marginHorizontal: 15 }}>
-                    <DropDownPicker
-                        open={open}
-                        value={language}
-                        items={items}
-                        setOpen={setOpen}
-                        setValue={setLanguage}
-                        setItems={setItems}
-                        placeholder={'언어 선택'}
-                        textStyle={{ fontFamily: 'Pretendard-Bold', fontSize: 17, color: '#5678F0' }}
-                        containerStyle={{ width: 130 }}
-                        style={{ borderWidth: 0 }}
-                        dropDownContainerStyle={{ borderWidth: 0 }}
-                        showArrowIcon={true}
-                        showTickIcon={false}
-                        ArrowDownIconComponent={() => <Entypo name="chevron-small-down" size={24} color="#5678F0" />}
-                        ArrowUpIconComponent={() => <Entypo name="chevron-small-up" size={24} color="#5678F0" />}
-                    />
-                </View>
-
-                <View style={{ marginHorizontal: 25, flexDirection: 'row', alignItems: 'center' }}>
-                    <Text style={style.GuideText}>채팅방명</Text>
-                    <TextInput style={style.InfoText} placeholder='채팅방명을 입력해 주세요.' />
-                </View>
-                <View style={{ marginHorizontal: 25, flexDirection: 'row', alignItems: 'center' }}>
-                    <Text style={style.GuideText}>최대정원</Text>
-                    <TextInput style={style.InfoText} keyboardType='number-pad' placeholder='채팅방 정원을 입력해 주세요.' />
-                </View>
-                <TextInput style={{ marginHorizontal: 25, marginTop: 18, ...style.TitleText }} placeholder='게시글 제목을 입력해 주세요.' />
+                <TextInput
+                    value={title}
+                    onChangeText={(text) => { setTitle(text) }}
+                    style={{ marginHorizontal: 25, marginTop: 18, ...style.TitleText }}
+                    placeholder='게시글 제목을 입력해 주세요.' />
                 <View style={style.HorizontalLine} />
-                <View style={{marginHorizontal: 25}}>
-                    <TextInput style={{ textAlignVertical: 'top', minHeight: 300, ...style.ContentText }} multiline={true} placeholder='스터디 내용을 입력해 주세요.' />
+                <View style={{ marginHorizontal: 25 }}>
+                    <TextInput
+                        value={content}
+                        onChangeText={(text) => { setContent(text) }}
+                        style={{ textAlignVertical: 'top', minHeight: 300, ...style.ContentText }}
+                        multiline={true}
+                        placeholder='스터디 내용을 입력해 주세요.' />
                 </View>
-                <View style={{marginHorizontal: 25}}>
+                <View style={{ marginHorizontal: 25 }}>
                     <Text style={{ ...style.InfoText }}>요일 선택</Text>
                     <MultiSelectButton
                         count={7}
                         textArray={['월', '화', '수', '목', '금', '토', '일']}
-                        getSelected={setSelectedWay}
+                        getSelected={setSelectedDays}
                         selectedButtonColor="#000000"
                         selectedTextColor="#ffffff"
                         containerStyle={{ flexDirection: 'row', marginTop: 5, marginBottom: 10 }}
@@ -90,7 +209,7 @@ export const BoardWriteScreen = () => {
                     <SingleSelectButton
                         count={2}
                         textArray={['대면', '비대면']}
-                        getSelected={setSelectedDays}
+                        getSelected={setSelectedWay}
                         selectedButtonColor="#000000"
                         selectedTextColor="#ffffff"
                         containerStyle={{ flexDirection: 'row', marginTop: 5, marginBottom: 10 }}
@@ -100,6 +219,7 @@ export const BoardWriteScreen = () => {
 
                     <View style={{ backgroundColor: '#5678F0', borderRadius: 16, marginTop: 10 }}>
                         <TouchableOpacity
+                            onPress={() => CreateStudy()}
                             style={{ justifyContent: 'center', alignItems: 'center' }}>
                             <Text style={{
                                 fontSize: 18,
@@ -111,6 +231,7 @@ export const BoardWriteScreen = () => {
                     </View>
                 </View>
             </ScrollView>
+            <Toast config={toastConfig} />
         </View>
     )
 }
@@ -162,5 +283,5 @@ const style = StyleSheet.create({
     SelectText: {
         fontFamily: 'Pretendard-Regular',
         fontSize: 14
-    }
+    },
 });
