4031f081f4bcb5a701b9aa8bc069dd12d3b7c09c
chaei <day3856@gmail.com>
2024-08-02 12:23:36 +0900

    Modifying calendar date notation

--- app/screens/EditProfileScreen.js
+++ app/screens/EditProfileScreen.js
@@ -85,10 +85,8 @@ export default function EditProfileScreen({ navigation }) {
   };
 
   const formatDate = (date) => {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+    return new Intl.DateTimeFormat('ko-KR', options).format(date);
   };
 
   return (
