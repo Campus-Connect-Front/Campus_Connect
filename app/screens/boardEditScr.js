c36076974de1f5a08e01beab7b9e32ccb60e17ad
hye33 <128382222+hye33@users.noreply.github.com>
2024-08-04 00:08:09 +0900

    [Feat] 게시글 작성, 편집 페이지 벗어날 경우 경고 모달 출력

--- app/screens/boardEditScr.js
+++ app/screens/boardEditScr.js
@@ -1,20 +1,23 @@
 import * as React from 'react';
 import * as SplashScreen from 'expo-splash-screen';
-import { Text, View, StyleSheet, TextInput, TouchableOpacity, Platform, Alert } from 'react-native';
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Platform, Alert, BackHandler } from 'react-native';
 import { useFonts } from 'expo-font';
 import { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 import SingleSelectButton from '../components/SingleSelectButton';
 import MultiSelectButton from '../components/MultiSelectButton';
 import Toast from 'react-native-toast-message';
-import { horizontalLineStyle, selectButtonStyle, toastConfig } from '../assets/styles/globalStyles';
+import { alertButtonStyle, horizontalLineStyle, selectButtonStyle, toastConfig } from '../assets/styles/globalStyles';
 import DoneButton from '../components/DoneButton';
 import { AntDesign } from '@expo/vector-icons';
+import AlertModal from '../components/AlertModal';
 
 const removeSpace = text => (text.replace(/\s/g, ''))
 
 export const BoardEditScreen = ({ item, navigation }) => {
     const { id, title, content, language, recruit, frequency, way, days } = item;
+
+    const [modalVisible, setModalVisible] = useState(false);
 
     const [editTitle, setEditTitle] = useState(item.title);
     const [editContent, setEditContent] = useState(item.content);
@@ -37,10 +40,20 @@ export const BoardEditScreen = ({ item, navigation }) => {
         navigation.setOptions({
             headerTitle: '',
             headerBackImage: () => (
-                <AntDesign name="close" size={24} color="#CACACA" />
+                <TouchableOpacity onPress={() => setModalVisible(true)}>
+                    <AntDesign name="close" size={24} color="#CACACA" />
+                </TouchableOpacity>
             )
         });
     }, [navigation]);
+
+    useEffect(() => {
+        BackHandler.addEventListener('hardwareBackPress', () => {
+            setModalVisible(true);
+            return true;
+        });
+
+    }, []);
 
     const EditStudy = () => {
         if (removeSpace(editTitle) == '') {
@@ -122,6 +135,23 @@ export const BoardEditScreen = ({ item, navigation }) => {
                 <DoneButton containerStyle={{ marginTop: 10 }} text='게시글 수정' onPress={() => EditStudy()} />
             </ScrollView>
             <Toast config={toastConfig}/>
+            <AlertModal
+                modalVisible={modalVisible}
+                setModalVisible={setModalVisible}
+                showCloseButton={false}
+                title={'경고'}
+                message={'페이지를 벗어날 경우\n작성 중인 내용이 사라집니다.'}
+                alertButtons={[{
+                    text: '확인',
+                    style: alertButtonStyle.default,
+                    onPress: () => { setModalVisible(false); navigation.pop(); }
+                }, {
+                    text: '취소',
+                    style: alertButtonStyle.destructive,
+                    onPress: () => { setModalVisible(false); }
+                }]}
+                onRequestClose={() => { (failedMatch) ? setModalVisible(false) : stopMatching(); }}
+            />
         </View>
     )
 }
