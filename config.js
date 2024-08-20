9b5376337abefc4ca53337c4294978d98ce63b2e
sbukkki <128692080+sbukkki@users.noreply.github.com>
2024-08-20 19:50:45 +0900

    [fix] 배포 ip주소 수정
    [fix] 배포 ip주소 수정

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 // 서버 API 주소 관리
 // 시작하기 전에 base url IP주소 수정하고 실행시키기
 
-const BASE_URL = "http://192.168.45.57:8090" 
+const BASE_URL = "http://13.209.69.251:8090" 
 
-
+3
 export const API = {
     USER:`${BASE_URL}/user`,
     CHAT:`${BASE_URL}/chat`,
